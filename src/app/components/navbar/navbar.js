11446c54f31c1ae4862f9b97eb4da55422e49486
DaudHTM <DaudHTM@gmail.com>
2023-08-03 16:18:33 -0400

    added about us

--- src/app/components/navbar/navbar.js
+++ src/app/components/navbar/navbar.js
@@ -1,6 +1,15 @@
+"use client"; // next js kinda weird need to add this.
 import styles from './component.module.css'
+import { Link, animateScroll as scroll } from "react-scroll";
 
 export default function NavBar() {
+
+  const scrollToComponent = (id) => {
+    const targetComponent = document.getElementById(id); // Adjust the ID as needed
+    if (targetComponent) {
+      targetComponent.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
   return (
     <nav className={styles.navbar}>
       <a href='/' className={styles.name}><span>&gt;</span>Pyoneers</a>
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,32 @@
+"use client";
 import Image from 'next/image'
+import { useEffect, useState } from 'react';
 import styles from './page.module.css'
 
 export default function Home() {
+
+  function ScrollPosition() {
+    const [scrollY, setScrollY] = useState(0);
+  
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+  
+    useEffect(() => {
+
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []); 
+  
+  }
   return (
+   
     <main className={styles.main}>
       <div className={styles.homecontainer}>
+
         <img src="/logo.png" className={styles.logo} />
         <div>
           <span className={styles.title}>CHS Pyoneers</span>
@@ -16,17 +38,29 @@ export default function Home() {
         </div>
       </div>
       <div className={styles.content}>
-        <h1>Header Text</h1>
-        <h2>Header Text</h2>
-        <h3>Header Text</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida quis. Proin libero nunc consequat interdum varius. Ut sem nulla pharetra diam sit. Etiam sit amet nisl purus in mollis nunc. Eros in cursus turpis massa tincidunt. Id consectetur purus ut faucibus pulvinar elementum integer. Sit amet tellus cras adipiscing. Luctus accumsan tortor posuere ac ut. Sagittis eu volutpat odio facilisis mauris sit amet. Pretium lectus quam id leo in vitae turpis. Volutpat lacus laoreet non curabitur gravida arcu ac. Pellentesque adipiscing commodo elit at imperdiet dui accumsan. Ultrices dui sapien eget mi proin. Non curabitur gravida arcu ac tortor dignissim. Amet nisl suscipit adipiscing bibendum est. Non curabitur gravida arcu ac tortor. Sed egestas egestas fringilla phasellus.
-        </p>
+ 
+    <div className={styles.aboutcontainer}>
 
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent tristique magna sit amet purus gravida quis. Proin libero nunc consequat interdum varius. Ut sem nulla pharetra diam sit. Etiam sit amet nisl purus in mollis nunc. Eros in cursus turpis massa tincidunt. Id consectetur purus ut faucibus pulvinar elementum integer. Sit amet tellus cras adipiscing. Luctus accumsan tortor posuere ac ut. Sagittis eu volutpat odio facilisis mauris sit amet. Pretium lectus quam id leo in vitae turpis. Volutpat lacus laoreet non curabitur gravida arcu ac. Pellentesque adipiscing commodo elit at imperdiet dui accumsan. Ultrices dui sapien eget mi proin. Non curabitur gravida arcu ac tortor dignissim. Amet nisl suscipit adipiscing bibendum est. Non curabitur gravida arcu ac tortor. Sed egestas egestas fringilla phasellus.
-        </p>
+    <div className={styles.aboutInfoContainer}>
+
+    <h1 className={styles.title}>About Us</h1>
+
+    <h3 className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus minus quis deleniti voluptatum esse ea, commodi delectus porro repellat aspernatur. Iste animi eligendi rerum nisi alias consequatur quas maxime sint.
+    Aliquam veritatis laudantium dolor culpa, error natus reprehenderit quisquam, autem voluptate possimus pariatur minus nobis enim neque ab a maiores, reiciendis dicta quasi magnam unde illo. Tenetur minima iste rerum.</h3>
+
+    <a href='/about' className={styles.learnMoreBtn}>Learn More!</a>
+    </div>
+
+    <div className={styles.pythonImageContainer}>
+    <img src="/pythonLogo.png" className={styles.logo} />
+    
+    </div>
+
+    </div>
+      
+   
       </div>
     </main>
-  )
+
+   )
 }
