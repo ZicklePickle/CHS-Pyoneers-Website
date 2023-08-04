742d56cd06f0bd48504db160f2e6704852c4bc78
DaudHTM <DaudHTM@gmail.com>
2023-08-03 20:55:29 -0400

    added sick transitions

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import NavBar from './components/navbar/navbar'
 import Footer from './components/footer/footer'
-
+import { RevealAnimation } from './components/revealAnimation/RevealAnimation'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -14,9 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      
         <section>
+       
           <NavBar></NavBar>
+       
         </section>
+        
         <section className='page'>
           {children}
         </section>
--- src/app/page.js
+++ src/app/page.js
@@ -2,61 +2,64 @@
 import Image from 'next/image'
 import { useEffect, useState } from 'react';
 import styles from './page.module.css'
-
+import {motion,useInView} from 'framer-motion';
+import {RevealAnimation} from "./components/revealAnimation/RevealAnimation"
 export default function Home() {
 
-  function ScrollPosition() {
-    const [scrollY, setScrollY] = useState(0);
   
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
-    };
-  
-    useEffect(() => {
 
-      window.addEventListener('scroll', handleScroll);
   
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []); 
   
-  }
   return (
    
     <main className={styles.main}>
       <div className={styles.homecontainer}>
-
+        <RevealAnimation>
         <img src="/logo.png" className={styles.logo} />
+        </RevealAnimation>
         <div>
+        <RevealAnimation>
           <span className={styles.title}>CHS Pyoneers</span>
           <p>Some subtitle text goes here...</p>
           <div className={styles.btngroup}>
               <button className='btn-primary'>Log Attendance</button>
               <button className='btn-secondary'>View Credits</button>
           </div>
+          </RevealAnimation>
         </div>
+        
       </div>
       <div className={styles.content}>
  
     <div className={styles.aboutcontainer}>
 
     <div className={styles.aboutInfoContainer}>
-
+<RevealAnimation>
     <h1 className={styles.title}>About Us</h1>
+</RevealAnimation>
 
+<RevealAnimation>
     <h3 className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus minus quis deleniti voluptatum esse ea, commodi delectus porro repellat aspernatur. Iste animi eligendi rerum nisi alias consequatur quas maxime sint.
     Aliquam veritatis laudantium dolor culpa, error natus reprehenderit quisquam, autem voluptate possimus pariatur minus nobis enim neque ab a maiores, reiciendis dicta quasi magnam unde illo. Tenetur minima iste rerum.</h3>
 
     <a href='/about' className={styles.learnMoreBtn}>Learn More!</a>
+    </RevealAnimation>
     </div>
-
+    
     <div className={styles.pythonImageContainer}>
-    <img src="/pythonLogo.png" className={styles.logo} />
+    <motion.img animate={{
+        rotate: 360, 
+      }}
+      transition={{
+        repeat: Infinity, 
+        duration: 9, 
+        ease: 'linear', 
+      }} src="/pythonLogo.png" className={styles.logo} />
     
     </div>
 
     </div>
+   
       
    
       </div>
