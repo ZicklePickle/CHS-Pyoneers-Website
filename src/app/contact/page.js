90595a0b5ccc6b3c911f76b8fe5c823de6c5f309
Ramy K <myramkadd@gmail.com>
2023-08-10 19:35:44 -0400

    first build + some fixes

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -19,11 +19,9 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.homecontainer}>
         <div className={styles.textContainer}>
-          <RevealAnimation>
-            <span className={styles.title}>Contact Us</span>
-            <p className={styles.subtext}>Have questions or want to reach out? Contact us via <span id="typewriter" className={styles.typewriter}></span></p>
+          <span className={styles.title}>Contact Us</span>
+            <p className={styles.subtext}>Have questions or want to reach out? Contact us via<br /><span id="typewriter" className={styles.typewriter}></span></p>
             <p className={styles.responseTime}>Replies can take anywhere from 1-2 days; please join our Discord and DM/ping us for the fastest reply time.</p>
-          </RevealAnimation>
         </div>
         <div className={styles.buttonsContainer}>
           <a href="https://www.instagram.com/chspyoneers/" target="_blank" className={`${styles.btn} ${styles.instagram}`}>
--- src/app/page.js
+++ src/app/page.js
@@ -2,68 +2,53 @@
 import Image from 'next/image'
 import { useEffect, useState } from 'react';
 import styles from './page.module.css'
-import {motion,useInView} from 'framer-motion';
-import {RevealAnimation} from "./components/revealAnimation/RevealAnimation"
+import { motion, useInView } from 'framer-motion';
+import { RevealAnimation } from "./components/revealAnimation/RevealAnimation"
 export default function Home() {
-
-  
-
-  
-  
   return (
-   
+
     <main className={styles.main}>
       <div className={styles.homecontainer}>
-        <RevealAnimation>
         <img src="/logo.png" className={styles.logo} />
-        </RevealAnimation>
         <div>
-        <RevealAnimation>
           <span className={styles.title}>CHS Pyoneers</span>
           <p>Some subtitle text goes here...</p>
           <div className={styles.btngroup}>
-              <button className='btn-primary'>Log Attendance</button>
-              <button className='btn-secondary'>View Credits</button>
+            <button className='btn-primary'>Log Attendance</button>
+            <button className='btn-secondary'>View Credits</button>
           </div>
-          </RevealAnimation>
         </div>
-        
       </div>
       <div className={styles.content}>
- 
-    <div className={styles.aboutcontainer}>
 
-    <div className={styles.aboutInfoContainer}>
-<RevealAnimation>
-    <h1 className={styles.title}>About Us</h1>
-</RevealAnimation>
+        <div className={styles.aboutcontainer}>
 
-<RevealAnimation>
-    <h3 className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus minus quis deleniti voluptatum esse ea, commodi delectus porro repellat aspernatur. Iste animi eligendi rerum nisi alias consequatur quas maxime sint.
-    Aliquam veritatis laudantium dolor culpa, error natus reprehenderit quisquam, autem voluptate possimus pariatur minus nobis enim neque ab a maiores, reiciendis dicta quasi magnam unde illo. Tenetur minima iste rerum.</h3>
+          <div className={styles.aboutInfoContainer}>
+            <RevealAnimation>
+              <h1 className={styles.title}>About Us</h1>
+            </RevealAnimation>
 
-    <a href='/about' className={styles.learnMoreBtn}>Learn More!</a>
-    </RevealAnimation>
-    </div>
-    
-    <div className={styles.pythonImageContainer}>
-    <motion.img animate={{
-        rotate: 360, 
-      }}
-      transition={{
-        repeat: Infinity, 
-        duration: 9, 
-        ease: 'linear', 
-      }} src="/pythonLogo.png" className={styles.logo} />
-    
-    </div>
+            <RevealAnimation>
+              <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus minus quis deleniti voluptatum esse ea, commodi delectus porro repellat aspernatur. Iste animi eligendi rerum nisi alias consequatur quas maxime sint.
+                Aliquam veritatis laudantium dolor culpa, error natus reprehenderit quisquam, autem voluptate possimus pariatur minus nobis enim neque ab a maiores, reiciendis dicta quasi magnam unde illo. Tenetur minima iste rerum.</p>
 
-    </div>
-   
-      
-   
+              <a href='/about' className={styles.learnMoreBtn}>Learn More!</a>
+            </RevealAnimation>
+          </div>
+
+          <div className={styles.pythonImageContainer}>
+            <motion.img animate={{
+              rotate: 360,
+            }}
+              transition={{
+                repeat: Infinity,
+                duration: 9,
+                ease: 'linear',
+              }} src="/pythonLogo.png" className={styles.logo} />
+          </div>
+        </div>
       </div>
     </main>
 
-   )
+  )
 }
