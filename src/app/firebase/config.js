348ac0f13424a3a92023eeccd1a01b3118d4a3fe
DaudHTM <daudhtm@gmail.com>
2023-08-14 15:02:10 -0400

    updated firebase to pyoneer gmail

--- src/app/firebase/config.js
+++ src/app/firebase/config.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp,getApps } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,19 +7,19 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAuXD--TC47HNVL2MWPrt8yJz39UWAbvtc",
-  authDomain: "chspyoneerswebsite.firebaseapp.com",
-  projectId: "chspyoneerswebsite",
-  storageBucket: "chspyoneerswebsite.appspot.com",
-  messagingSenderId: "751197503950",
-  appId: "1:751197503950:web:36930508a62377da53ab9f",
-  measurementId: "G-2KE8JE80ES"
+  apiKey: "AIzaSyAwwlG7HfMzLvXTbjBwyJvjbtbNfOlEHz0",
+  authDomain: "chspyoneerswebsite-93c7c.firebaseapp.com",
+  projectId: "chspyoneerswebsite-93c7c",
+  storageBucket: "chspyoneerswebsite-93c7c.appspot.com",
+  messagingSenderId: "437432821867",
+  appId: "1:437432821867:web:51f266c6871c320236692e",
+  measurementId: "G-QW1NJZKY0E"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+let firebase = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
 export default firebase;
