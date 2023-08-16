4d13dab75fa00255e4a9d61320d43ea6b8ed93d5
CHS Pyoneers <80214121+CHS-Pyoneers@users.noreply.github.com>
2023-08-15 22:26:55 -0400

    remove old env reference

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
 /** @type {import('next').NextConfig} */
 const fs = require('fs');
 
-const dotenv = require('dotenv');
-
-const env = dotenv.parse(fs.readFileSync('.env'));
-
 const nextConfig = {
     output: "export",
     basePath: "/Website",
     images: {
         unoptimized: true
-    },
-    env:env
+    }
 }
 
 module.exports = nextConfig
