1b3c82c2232217e7e739f22f3dd6cf19ac191c18
Ramy K <myramkadd@gmail.com>
2023-08-19 22:10:53 -0400

    leaderboard finished

--- src/app/leaderboard/page.js
+++ src/app/leaderboard/page.js
@@ -1,33 +1,55 @@
 "use client"
 import { useState, useEffect } from "react"
-import {getAllUser} from "../services/userService"
+import {getAllUser, getUser} from "../services/userService"
 import styles from "./page.module.css"
 
 export default function Page() {
-    let [users, setUsers] = useState([])
+    let [leaderboard, setLeaderboard] = useState([])
 
     useEffect(() => {
-        console.log(users)
-        if(users.length == 0) {
-            getAllUser().then(u => {
-                console.log(u)
-                u.sort((a, b) => calcCredits(a) - calcCredits(b))
-                setUsers(u)
+        if(leaderboard.length == 0) {
+            getAllUser().then(users => {
+                fetchCredits(users).then(lb => {
+                    console.log(lb)
+                    setLeaderboard(lb)
+                })
             })
         }
-    }, [users])
+    }, [])
 
     function calcCredits(user) {
-        const creds = user.creditsBreakdown.reduce((accum, curVal) => accum + curVal)
+        const creds = user.creditsBreakdown.reduce((accum, curVal) => accum + curVal.credits, 0)
+        console.log(creds)
         return creds
+    }
+
+    async function fetchCredits(users) {
+        let userCredits = []
+        for(const [name, id] of Object.entries(users)) {
+            let userData = (await getUser(id)).data();
+            userCredits.push({
+                name: name,
+                credits: calcCredits(userData)
+            })
+        }
+
+        return userCredits.sort((a, b) => a.credits - b.credits);
     }
 
     return (
         <main className={styles.main}>
-            <div>
-                <ol>
-                    
-                </ol>
+            <div className={styles.container}>
+                <div className={styles.header}>
+                    <h1 className={styles.headertext}>Credit Leaderboard</h1>
+                </div>
+                <div className={styles.leaderboard}>
+                {
+                    leaderboard.map((entry, index) => 
+                        <p>{index+1}. {entry.name} - {entry.credits} credits</p>
+                    )
+                }
+                </div>
+                
             </div>
         </main>
     )
