a7bcc4c4ff2dc9ca43a0dc36b131c1067e96547f
aryans0714 <aryan.sharma0714@gmail.com>
2023-08-20 13:04:47 -0400

    about

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,49 +1,73 @@
 "use client";
 import Image from 'next/image'
 import inherited_styles from '../page.module.css'
+import styles from './page.module.css'
 import { motion } from 'framer-motion';
 import { RevealAnimation } from "../components/revealAnimation/RevealAnimation"
 import Link from 'next/link';
 
 export default function Home() {
   return (
-
     <main className={inherited_styles.main}>
-      <div className={inherited_styles.content}>
+      <div className={styles.content}>
 
-        <div className={inherited_styles.aboutcontainer}>
-
+        <div className={styles.aboutcontainer}>
           <div className={inherited_styles.aboutInfoContainer}>
             <RevealAnimation>
-              <h1 className={inherited_styles.title}>Our Mission</h1>
+              <h1 className={inherited_styles.title}>What We Do</h1>
             </RevealAnimation>
-
             <RevealAnimation>
-              <p className={inherited_styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Praesent tristique magna sit amet purus gravida quis. Proin libero nunc consequat interdum varius. 
-                    Ut sem nulla pharetra diam sit. Etiam sit amet nisl purus in mollis nunc. Eros in cursus turpis massa tincidunt. 
-                    Id consectetur purus ut faucibus pulvinar elementum integer. Sit amet tellus cras adipiscing. 
-                    Luctus accumsan tortor posuere ac ut. Sagittis eu volutpat odio facilisis mauris sit amet. 
-                    Pretium lectus quam id leo in vitae turpis. Volutpat lacus laoreet non curabitur gravida arcu ac. 
-                    Pellentesque adipiscing commodo elit at imperdiet dui accumsan. Ultrices dui sapien eget mi proin. 
-                    Non curabitur gravida arcu ac tortor dignissim. Amet nisl suscipit adipiscing bibendum est. 
-                    Non curabitur gravida arcu ac tortor. Sed egestas egestas fringilla phasellus.</p>
+              <p className={inherited_styles.text}>
+                The Pyoneers Club leads two main efforts: teaching and organizing competitions.
+                We currently lead beginner and advanced lessons at the following schools:
+                <br/><br/>&nbsp;{' > '}Burleigh Manor Middle School
+                <br/>&nbsp;{' > '}Dunloggin Middle School
+                <br/>&nbsp;{' > '}Centennial High School
+                <br/><br/>and have been doing so since 2020! Topics involving anything from basic print statements to advanced object-oriented principles have been covered in these lessons. Alongside that, we have organized three Centennial Informatics Tournaments (CInTs), where contestants solve USACO-esque problems for cash prizes.
+              </p>
             </RevealAnimation>
           </div>
-
           <div className={inherited_styles.pythonImageContainer}>
-            <motion.img animate={{
-              rotate: 360,
-            }}
-              transition={{
-                repeat: Infinity,
-                duration: 9,
-                ease: 'linear',
-              }} src="/Website/pythonLogo.png" className={inherited_styles.logo} />
+            <img src="/Website/logo.png" className={inherited_styles.logo} />
+          </div>
+        </div>
+
+        <div className={styles.aboutcontainer}>
+          <div className={inherited_styles.aboutInfoContainer}>
+            <RevealAnimation>
+              <h1 className={inherited_styles.title} style={{textAlign: 'center'}}>Board</h1>
+            </RevealAnimation>
+            <RevealAnimation>
+              <p className={inherited_styles.text}>
+                Put headshots and bios here
+              </p>
+            </RevealAnimation>
+          </div>
+        </div>
+
+        <div className={styles.aboutcontainer}>
+          <div className={inherited_styles.committee}>
+            <RevealAnimation>
+              <h1 className={inherited_styles.title} style={{textAlign: 'center'}}>Education Committee</h1>
+            </RevealAnimation>
+            <RevealAnimation>
+              <p className={inherited_styles.text} style={{textAlign: 'center'}}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  
+              </p>
+            </RevealAnimation>
+          </div>
+          <div className={inherited_styles.committee}>
+            <RevealAnimation>
+              <h1 className={inherited_styles.title} style={{textAlign: 'center'}}>Tournament Committee</h1>
+            </RevealAnimation>
+            <RevealAnimation>
+              <p className={inherited_styles.text} style={{textAlign: 'center'}}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              </p>
+            </RevealAnimation>
           </div>
         </div>
       </div>
     </main>
-
   )
 }
